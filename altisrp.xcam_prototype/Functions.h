d28820b3896f09411d459cd746948fcad987ac2f
Youri-Litovski <aaalex123@hotmail.fr>
2015-02-20 00:09:54 +0100

    Requete medic anonyme

--- altisrp.xcam_prototype/Functions.h
+++ altisrp.xcam_prototype/Functions.h
@@ -66,6 +66,7 @@ class Life_Client_Core
 		class revived {};
 		class medicMarkers {};
 		class requestMedic {};
+		class requestMedic_Anon {};
 		class medicRequest {};
 		class deathScreen {};
 		class medicLoadout {};
