f444cf2d1b20d3a2535de3c37fb3b8ea60d16d31
havena <silveredenis@gmail.com>
2015-05-02 01:15:53 +0200

    pbo

--- PBO a rajouter/sign_altisrpfr.bat
+++ PBO a rajouter/sign_altisrpfr.bat
@@ -1,5 +1,3 @@
 cd "C:\Users\sil\Documents\GitHub\altislife\tool"
-DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\xCam_Woodworks.pbo"
-DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\xCam_Metal.pbo"
-DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\ice_apo_seekers.pbo"
+DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\xCam_EU.pbo"
 PAUSE
