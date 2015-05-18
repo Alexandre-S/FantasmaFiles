3298cc32b9f4af3df42861fdc3b9acd62da2dc16
havena <silveredenis@gmail.com>
2015-05-18 13:48:53 +0200

    menottes si X ace_captives

--- @ace/addons/sign_altisrpfr.bat
+++ @ace/addons/sign_altisrpfr.bat
@@ -1,4 +1,6 @@
 cd "C:\Users\sil\Documents\GitHub\altislife\tool"
 DSSignFile.exe havena.biprivatekey C:\Users\sil\Documents\GitHub\altislife\@ace\addons\ace_interaction.pbo
 
+DSSignFile.exe havena.biprivatekey C:\Users\sil\Documents\GitHub\altislife\@ace\addons\ace_captives.pbo
+
 PAUSE
