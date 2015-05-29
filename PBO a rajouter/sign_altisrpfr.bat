2d2f69eb96aaa86aa7ca30d8d0e9e73322d1d1f4
havena <silveredenis@gmail.com>
2015-05-30 00:45:47 +0200

    ok

--- PBO a rajouter/sign_altisrpfr.bat
+++ PBO a rajouter/sign_altisrpfr.bat
@@ -1,3 +1,3 @@
 cd "C:\Users\sil\Documents\GitHub\altislife\tool"
-DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\bzly_mantrucks.pbo"
+DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\Skyline_Mercedes_300_SEL.pbo"
 PAUSE
