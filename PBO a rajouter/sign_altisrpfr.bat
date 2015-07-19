f4053e01fd7b58a056e0f6acff31acd3198a5588
havena <silveredenis@gmail.com>
2015-07-19 18:32:48 +0200

    fix

--- PBO a rajouter/sign_altisrpfr.bat
+++ PBO a rajouter/sign_altisrpfr.bat
@@ -1,3 +1,5 @@
 cd "C:\Users\sil\Documents\GitHub\altislife\tool"
-DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\kio_fhat.pbo"
+DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\Skyline_Mercedes_300_SEL.pbo"
+DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\HAFM_EC635.pbo"
+DSSignFile.exe havena.biprivatekey "C:\Users\sil\Documents\GitHub\altislife\PBO a rajouter\HAFM_EC635_Config.pbo"
 PAUSE
